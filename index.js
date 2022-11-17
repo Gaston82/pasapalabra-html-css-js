414ada41bc7197cc11a704807fbabc8f1d269277
Gaston82 <cabezaartaza@hotmail.com>
2022-11-18 00:45:14 +0100

    add ranking to the game
    get points from local storage
    
    handle the ranking table by classes

--- index.js
+++ index.js
@@ -176,19 +176,6 @@ const questions = [
   },
 ];
 
-const sectionRules = document.getElementsByClassName("game-rules-section");
-const buttonStartGame = document.getElementsByClassName("button");
-const inputPlayerName = document.getElementById("player-name");
-const gameContainer = document.getElementsByClassName("game-container");
-const container = document.getElementById("rosco-container");
-const displayQuestions = document.getElementById("display-questions");
-const questionsParagraph = document.querySelector(".questions");
-const input = document.getElementById("input");
-const buttonAnswer = document.getElementById("buttonAnswer");
-const pasapalabraButton = document.getElementById("pasapalabra");
-const formAnswers = document.getElementById("form-answers");
-const letterCircle = document.getElementsByClassName("circle");
-const displayCorrectAnswered = document.getElementById("correctAnswered");
 const letters = [
   "A",
   "B",
@@ -218,6 +205,22 @@ const letters = [
   "Y",
   "Z",
 ];
+
+const gameTitle = document.getElementsByClassName("game-title");
+const sectionRules = document.getElementsByClassName("game-rules-section");
+const buttonStartGame = document.getElementsByClassName("button");
+const inputPlayerName = document.getElementById("player-name");
+const gameContainer = document.getElementsByClassName("game-container");
+const container = document.getElementById("rosco-container");
+const displayQuestions = document.getElementById("display-questions");
+const questionsParagraph = document.querySelector(".questions");
+const input = document.getElementById("input");
+const buttonAnswer = document.getElementById("buttonAnswer");
+const pasapalabraButton = document.getElementById("pasapalabra");
+const formAnswers = document.getElementById("form-answers");
+const letterCircle = document.getElementsByClassName("circle");
+const displayCorrectAnswered = document.getElementById("correctAnswered");
+const rankingTable = document.getElementsByClassName("ranking-table");
 
 let newPLayer = {
   playerName: "",
@@ -253,153 +256,138 @@ let wrongAnswer = 0;
 let totalAnswered = 0;
 let answerPlayer;
 
-
-let rankingTable = document.getElementById("ranking-table");
-function createRanking(){
-
-  // const{name,playerName,points,correctAnswer,wrongAnswer}=ranking
-  ranking.forEach((player,index)=>{
+function createRanking(points) {
+  points.forEach((player, index) => {
     let rowTable = document.createElement("tr");
     let columnPosition = document.createElement("td");
     let columnPoints = document.createElement("td");
     let columnName = document.createElement("td");
     let columnCorrect = document.createElement("td");
     let columnWrong = document.createElement("td");
-    columnPosition.textContent=index+1;
-    columnName.textContent=player.playerName;
-    columnPoints.textContent=player.points;
-    columnCorrect.textContent=player.correctAnswers;
-    columnWrong.textContent=player.wrongAnswers;
-    
+    columnPosition.textContent = index + 1;
+    columnName.textContent = player.playerName;
+    columnPoints.textContent = player.points;
+    columnCorrect.textContent = player.correctAnswers;
+    columnWrong.textContent = player.wrongAnswers;
+
     rowTable.appendChild(columnPosition);
     rowTable.appendChild(columnName);
     rowTable.appendChild(columnPoints);
     rowTable.appendChild(columnCorrect);
     rowTable.appendChild(columnWrong);
-     rankingTable.appendChild(rowTable)
-  })
+    rankingTable[0].appendChild(rowTable);
+  });
+}
+
+formAnswers.addEventListener("submit", (e) => {
+  e.preventDefault();
+  nextQuestion(input.value);
+});
+buttonStartGame[0].addEventListener("click", startGame);
+// pasapalabraButton.addEventListener("click", nextQuestion);
+displayCorrectAnswered.textContent = correctAnswer;
+
+function startGame() {
+  console.log(gameTitle[0].textContent);
+  newPLayer.playerName = inputPlayerName.value;
+  sectionRules[0].classList.replace("game-rules-section", "hidden");
+  gameContainer[0].classList.remove("hidden");
+}
+
+function nextQuestion() {
+  checkAnswer();
+  if (position < questions.length) {
+    position++;
   }
-  
-   createRanking();
+  if (position === questions.length) {
+    checkTotalAnswered();
+  }
 
-// formAnswers.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   nextQuestion(input.value);
-// });
-// buttonStartGame[0].addEventListener("click", startGame);
-// // pasapalabraButton.addEventListener("click", nextQuestion);
-// displayCorrectAnswered.textContent = correctAnswer;
+  input.value = "";
+  showQuestions();
+}
 
-// function startGame() {
-//   newPLayer.playerName = inputPlayerName.value;
-//   sectionRules[0].classList.replace("game-rules-section", "hidden");
-//   gameContainer[0].classList.remove("hidden");
-// }
+const createRosco = () => {
+  for (let i = 0; i < letters.length; i++) {
+    let circle = document.createElement("div");
+    circle.textContent = letters[i];
+    circle.classList.add("circle");
+    container.appendChild(circle);
+  }
+};
 
-// function nextQuestion() {
-//   checkAnswer();
-//   if (position < questions.length) {
-//     position++;
-//   }
-//   if (position === questions.length) {
-//     checkTotalAnswered();
-//   }
+const showQuestions = () => {
+  for (let i = position; i < questions.length; i++) {
+    if (questions[i].status === 0) {
+      questionsParagraph.textContent = questions[i].question;
+      break;
+    }
+    if (questions[i].status === 3) {
+      position = i;
+      questionsParagraph.textContent = questions[i].question;
+      break;
+    }
+  }
+};
 
-//   input.value = "";
-//   showQuestions();
-// }
+const checkAnswer = () => {
+  answerPlayer = input.value;
+  let pasapalabraOption = "";
+  // let pasapalabraOption = pasapalabraButton.textContent;
 
-// const createRosco = () => {
-//   for (let i = 0; i < letters.length; i++) {
-//     let circle = document.createElement("div");
-//     circle.textContent = letters[i];
-//     circle.classList.add("circle");
-//     container.appendChild(circle);
-//   }
-// };
+  if (answerPlayer === questions[position].answer) {
+    questions[position].status = 1;
+    letterCircle[position].classList.add("green");
+    totalAnswered++;
+    newPLayer.correctAnswers += 1;
+    displayCorrectAnswered.textContent = correctAnswer;
+  } else if (answerPlayer === "pasapalabra") {
+    questions[position].status = 3;
+  } else {
+    questions[position].status = 1;
+    letterCircle[position].classList.add("red");
+    totalAnswered++;
+    newPLayer.wrongAnswers += 1;
+  }
+};
 
-// const showQuestions = () => {
-//   for (let i = position; i < questions.length; i++) {
-//     if (questions[i].status === 0) {
-//       questionsParagraph.textContent = questions[i].question;
-//       break;
-//     }
-//     if (questions[i].status === 3) {
-//       position = i;
-//       questionsParagraph.textContent = questions[i].question;
-//       break;
-//     }
-//   }
-// };
+const checkTotalAnswered = () => {
+  if (totalAnswered === questions.length) {
+    let totalPoints = playerPoints(newPLayer.correctAnswers);
+    gameContainer[0].classList.replace("game-container", "hidden");
+    rankingTable[0].classList.replace("hidden", "ranking-table");
+    let rankingFromLocalStorage = getRankingFromLocalStorage(totalPoints);
+    gameTitle[0].textContent = "Ranking";
+    createRanking(rankingFromLocalStorage);
+  } else {
+    position = 0;
+  }
+};
 
-// const checkAnswer = () => {
-//   answerPlayer = input.value;
-//   let pasapalabraOption = "";
-//   // let pasapalabraOption = pasapalabraButton.textContent;
+const playerPoints = (correctAnswers) => {
+  newPLayer.points = correctAnswers;
+  ranking.push(newPLayer);
+  const rankingSorted = ranking.sort(function (a, b) {
+    return b.points - a.points;
+  });
 
-//   if (answerPlayer === questions[position].answer) {
-//     questions[position].status = 1;
-//     letterCircle[position].classList.add("green");
-//     totalAnswered++;
-//     newPLayer.correctAnswers+=1;
-//     displayCorrectAnswered.textContent = correctAnswer;
-//   } else if (answerPlayer === "pasapalabra") {
-//     questions[position].status = 3;
-//   } else {
-//     questions[position].status = 1;
-//     letterCircle[position].classList.add("red");
-//     totalAnswered++;
-//     newPLayer.wrongAnswers+=1;
-//   }
-// };
+  saveRankingLocalStorage(rankingSorted);
 
+  return rankingSorted;
+};
 
-// const checkTotalAnswered = () => {
-//   if (totalAnswered === questions.length) {
-//     console.log("has terminado el juego");
-//   } else {
-//     position = 0;
-//   }
-// };
+function saveRankingLocalStorage(ranking) {
+  localStorage.setItem("ranking", JSON.stringify(ranking));
+}
 
-// const playerPoints = (correctAnswers) => {
-//   newPLayer.points = correctAnswers;
-//   ranking.push(newPLayer);
-//   const rankingSorted = ranking.sort(function (a, b) {
-//     return b.points - a.points;
-//   });
- 
-//   saveRankingLocalStorage(rankingSorted);
-  
-//   return rankingSorted;
-// };
+function getRankingFromLocalStorage(ranking) {
+  ranking = JSON.parse(localStorage.getItem("ranking"));
 
-// function saveRankingLocalStorage(ranking){
+  return ranking;
+}
 
-//   localStorage.setItem("ranking",JSON.stringify(ranking));
-// }
+const welcomePlayer = () => {};
 
-// function getRankingFromLocalStorage(ranking){
-
-//   let points = localStorage.getItem("ranking",JSON.parse(ranking));
-
-//   console.log(showRanking(points));
-
-// }
-
-
-
-// const showRanking =(sortedRanking)=>{
-
-//   sortedRanking.forEach((player,index)=>{
-//     console.log(`Position ${index + 1}: ${player.playerName}\n Points: ${player.points}\n Correct Answers: ${player.correctAnswers} \n Wrong Answers: ${player.wrongAnswers}`)
-//     })
-
-
-// }
-
-// const welcomePlayer = () => {};
-
-// welcomePlayer();
-// createRosco();
-// showQuestions();
+welcomePlayer();
+createRosco();
+showQuestions();
