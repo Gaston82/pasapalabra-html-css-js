9f1e864c39e3fe2b27d2afa70a693f2f962904fa
Gaston82 <cabezaartaza@hotmail.com>
2022-11-23 00:32:28 +0100

    feature: add random questions functionallity

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const questions = [
+const questionsList = [[
   {
     letter: "a",
     answer: "abducir",
@@ -174,7 +174,63 @@ const questions = [
     question:
       "CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional",
   },
-];
+],[
+  { letter: "a", answer: "alicia", status: 0, pasapalabra: false, question: "CON LA A. Niña que fue al pais de las maravillas" },
+  { letter: "b", answer: "bambi", status: 0, pasapalabra: false, question: "CON LA B. Cervatillo de pelicula que se queda sin su madre" },
+  { letter: "c", answer: "caperucita", status: 0, pasapalabra: false, question: "CON LA C. Personaje de cuento que le lleva una cesta a su abuela" },
+  { letter: "d", answer: "donald", status: 0, pasapalabra: false, question: "CON LA D. Pato amigo de Mickey" },
+  { letter: "e", answer: "elefante", status: 0, pasapalabra: false, question: "CON LA E. Animal que era Dumbo" },
+  { letter: "f", answer: "felices", status: 0, pasapalabra: false, question: "CON LA F. Los animales de cuento comen perdices y viven..." },
+  { letter: "g", answer: "genio", status: 0, pasapalabra: false, question: "CON LA G.  Lo que sale al frotar una lampara magica" },
+  { letter: "h", answer: "heidi", status: 0, pasapalabra: false, question: "CON LA H. Vivia en los Alpes con su abuelito" },
+  { letter: "i", answer: "increibles", status: 0, pasapalabra: false, question: "CON LA I. Familia de superheroes" },
+  { letter: "j", answer: "jones", status: 0, pasapalabra: false, question: "CON LA J. Apellido de Indiana" },
+  { letter: "k", answer: "king-kong", status: 0, pasapalabra: false, question: "CON LA K. Gorila gigante que vive en Nueva York" },
+  { letter: "l", answer: "lobo", status: 0, pasapalabra: false, question: "CON LA L. Animal del bosque que en los cuentos es el malo" },
+  { letter: "m", answer: "madagascar", status: 0, pasapalabra: false, question: "CON LA M. Isla africana donde llegan animales que se escapan del zoo" },
+  { letter: "n", answer: "nemo", status: 0, pasapalabra: false, question: "CON LA N. Dory y Marlin lo buscan" },
+  { letter: "enye", answer: "araña", status: 0, pasapalabra: false, question: "CON LA Ñ. Animal que le da superpoderes a un heroe" },
+  { letter: "o", answer: "ogro", status: 0, pasapalabra: false, question: "CON LA O. Lo que era Shreck." },
+  { letter: "p", answer: "peter pan", status: 0, pasapalabra: false, question: "CON LA P. Niño que nunca creia y que vivia en Nunca Jamas" },
+  { letter: "q", answer: "mcqueen", status: 0, pasapalabra: false, question: "CONTIENE LA Q. Apellido de Rayo, coche de carreras" },
+  { letter: "r", answer: "ratatouille", status: 0, pasapalabra: false, question: "CON LA R. Pelicula de una rata cocinera" },
+  { letter: "s", answer: "setas", status: 0, pasapalabra: false, question: "CON LA S. Las casas de los pitufos" },
+  { letter: "t", answer: "teletubbies", status: 0, pasapalabra: false, question: "CON LA T. Cuatro personajes que son de color: morado, amarillo, verde y rojo" },
+  { letter: "u", answer: "ursula", status: 0, pasapalabra: false, question: "CON LA U. Malvada de la Sirenita, tiene cuerpo de pulpo" },
+  { letter: "v", answer: "vikingo", status: 0, pasapalabra: false, question: "CON LA V. Vickie, ademas de inteligente y astuto era..." },
+  { letter: "w", answer: "whinnie", status: 0, pasapalabra: false, question: "CON LA W. Osito del bosque de los Cien Arces al que le encanta la miel" },
+  { letter: "x", answer: "x-men", status: 0, pasapalabra: false, question: "CON LA X. Grupo de superhéroes del que forman parte Lobezno, Tormenta,..." },
+  { letter: "y", answer: "yasmine", status: 0, pasapalabra: false, question: "CON LA Y. La princesa que Aladdin quiere conquistar" },
+  { letter: "z", answer: "zipi-zape", status: 0, question: "CON LA Z. Hermanos gemelos de los tebeos" }
+],[
+  { letter: "a", answer: "abecedario", status: 0, pasapalabra: false, question: "CON LA A. Conjunto de las letras de un idioma puestas en orden" },
+  { letter: "b", answer: "babero", status: 0, pasapalabra: false, question: "CON LA B. Tela que se pone a los bebés en el pecho para que no se manchen al comer" },
+  { letter: "c", answer: "cabeza", status: 0, pasapalabra: false, question: "CON LA C. Parte superior del cuerpo" },
+  { letter: "d", answer: "dormir", status: 0, pasapalabra: false, question: "CON LA D. Accion. Forma de descanso que hacemos por las noches o en la siesta con los ojos cerrados" },
+  { letter: "e", answer: "elefante", status: 0, pasapalabra: false, question: "CON LA E. Animal mamifero enorme que vive en Asia o Africa. Con grandes orejas y trompa " },
+  { letter: "f", answer: "fiebre", status: 0, pasapalabra: false, question: "CON LA F. Augmento de la temperatura del cuerpo cuando estamos enfermos" },
+  { letter: "g", answer: "globo", status: 0, pasapalabra: false, question: "CON LA G. Especie de pelota de goma que se hincha con aire. Les gusta mucho a los niños" },
+  { letter: "h", answer: "humo", status: 0, pasapalabra: false, question: "CON LA H. Lo que sale cuando se hace fuego." },
+  { letter: "i", answer: "inflar", status: 0, pasapalabra: false, question: "CON LA I. Llenar algo de aire" },
+  { letter: "j", answer: "jardin", status: 0, pasapalabra: false, question: "CON LA J. Zona que rodea algunas casas o edificios y que está lleno de césped, flores, árboles…" },
+  { letter: "k", answer: "kilo", status: 0, pasapalabra: false, question: "CON LA K. Medida de peso que es igual que 1000 gramos" },
+  { letter: "l", answer: "luna", status: 0, pasapalabra: false, question: "CON LA L. Satélite de la Tierra, de color blanco, que se ve por la noche" },
+  { letter: "m", answer: "mantel", status: 0, pasapalabra: false, question: "CON LA M. Tela que se pone en las mesas a la hora de comer" },
+  { letter: "n", answer: "nido", status: 0, pasapalabra: false, question: "CON LA N. Tipo de casa que construyen los pájaros para poner sus huevos" },
+  { letter: "enye", answer: "ñu", status: 0, pasapalabra: false, question: "CON LA Ñ. Mamífero africano de color marrón parecido a un toro con los cuernos curvos" },
+  { letter: "o", answer: "ojo", status: 0, pasapalabra: false, question: "CON LA O. Órgano del sentido de la vista. Lo que usamos para ver" },
+  { letter: "p", answer: "peces", status: 0, pasapalabra: false, question: "CON LA P. Animales que viven en el agua, cubiertos de escamas, con aletas para nadar" },
+  { letter: "q", answer: "quieto", status: 0, pasapalabra: false, question: "CON LA Q. Que no se mueve" },
+  { letter: "r", answer: "rapido", status: 0, pasapalabra: false, question: "CON LA R. Veloz" },
+  { letter: "s", answer: "sacapuntas", status: 0, pasapalabra: false, question: "CON LA S. Lo que usas para que tus lápices tengan la punta afilada" },
+  { letter: "t", answer: "tenedor", status: 0, pasapalabra: false, question: "CON LA T. Objeto que se usa para pinchar los alimentos y comérselos" },
+  { letter: "u", answer: "uña", status: 0, pasapalabra: false, question: "CON LA U. Parte dura que está en la punta de los dedos, que cortamos cuando crece y que las personas se pintan a veces" },
+  { letter: "v", answer: "verano", status: 0, pasapalabra: false, question: "CON LA V. Estación del año en la que hace mucho calor" },
+  { letter: "w", answer: "windsurf", status: 0, pasapalabra: false, question: "CON LA W. Deporte que se practica en el mar, de pie sobre una tabla alargada que lleva una vela triangular" },
+  { letter: "x", answer: "taxi", status: 0, pasapalabra: false, question: "CONTIENE LA X. Coche con conductor que lleva a las personas donde quieren ir. En Barcelona son negros y amarillos" },
+  { letter: "y", answer: "yate", status: 0, pasapalabra: false, question: "CON LA Y. Barco de lujo" },
+  { letter: "z", answer: "zoo", status: 0, pasapalabra: false, question: "CON LA Z. Parque en el que hay todo tipo de animales de todo el mundo que se pueden visitar por el público" }
+],];
 
 const letters = [
   "A",
@@ -227,6 +283,8 @@ const timeOverSection = document.getElementsByClassName("time-over-section");
 const timeOverMessage = document.getElementById("time-over-message");
 const buttonPlayAgain = document.getElementsByClassName("button-play-again");
 
+
+
 let newPLayer = {
   playerName: "",
   points: 0,
@@ -261,12 +319,15 @@ let wrongAnswer = 0;
 let totalAnswered = 0;
 let answerPlayer;
 
+let questions = questionsList[parseInt(Math.random() * (3-0))]
+
 const startGame = () => {
   sectionRules[0].classList.remove("game-rules-section");
   gameContainer[0].classList.remove("hidden");
-
+  showQuestions()
   gameTimer();
 };
+
 
 const createRanking = (points) => {
   points.forEach((player, index) => {
@@ -355,7 +416,6 @@ const checkAnswer = (answerPlayer) => {
   } else if (answerPlayer === "pasapalabra") {
     questions[position].status = 3;
   } else if (answerPlayer === "end") {
-    console.log("hello");
     gameContainer[0].classList.add("hidden");
     gameTitle[0].textContent = "You have left the game";
     timeOverMessage.textContent = `The player ${newPLayer.playerName} has ${newPLayer.correctAnswers} correct answers and made ${newPLayer.wrongAnswers} mistakes`;
